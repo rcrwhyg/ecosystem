89362bf352e5629fcf9e955876da16372ff14025
rcrwhyg <rcrwhyg@sina.com>
2024-09-22 00:40:52 +0800

    feature: introduce serde

---
+++ examples/serde1.rs
@@ -0,0 +1,13 @@
+// const KEY: &[u8] = b"01234567890123456789012345678901";
+
+// #[serde_as]
+// struct User {
+//     name: String,
+//     age: u8,
+//     skills: Vec<String>,
+//     data: Vec<u8>,
+//     sensitive: SensitiveData,
+//     url: Vec<http::Uri>,
+// }
+
+fn main() {}
